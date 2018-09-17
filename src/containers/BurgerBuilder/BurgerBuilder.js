01c11484817ed26a7df81f3bcbde0e7670036ba1
ZosiaSub <zofia.subocz@gmail.com>
2018-09-17 22:21:32 +0200

    Create main components skeleton

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Wrapper';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Burger</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
