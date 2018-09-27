66baee5127f109d4663df7dc834ee41c03cebf11
ZosiaSub <zofia.subocz@gmail.com>
2018-09-27 22:29:43 +0200

    Ingredients validation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    console.log(props);
-    const transformedIngredients = Object.keys(props.ingredients).map(iKey => 
+    let transformedIngredients = Object.keys(props.ingredients).map(iKey => 
         {
             return [...Array(props.ingredients[iKey])].map((_,i) => {
                  return <BurgerIngredient key={iKey + i} type={iKey} />
             }
                
             ) 
-        })
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        });
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please select your ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
