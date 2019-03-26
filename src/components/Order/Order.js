174b664e9ede92cd423c6dacce208b7a46de12ef
ZosiaSub <zofia.subocz@gmail.com>
2019-03-26 06:34:41 +0100

    pass the ingredients to the order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad(1)</p>
+const order = (props) => {
+    const ingredients = [];
+    for ( let ingredient in props.ingredients) {
+        ingredients.push({
+            name: ingredient,
+            amount: props.ingredients[ingredient]
+        });
+    }
+
+    const renderIngredients = ingredients.map(ig => (
+        <span 
+            key={ig}
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px',
+            }}
+        >{ig.name} ({ig.amount})</span>
+    ))
+
+    return (
+        <div className={classes.Order}>
+        <p>Ingredients: {renderIngredients}</p>
         <p>Price: <strong>EUR {Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
-)
+    )   
+}
 
 export default order;
