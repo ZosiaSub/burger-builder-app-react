bed96896c431cd3153c50b37ad9070335d4a9e76
ZosiaSub <zofia.subocz@gmail.com>
2019-03-27 07:06:18 +0100

    add new Input component and add to ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classses from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends React.Component {
     state = {
@@ -46,9 +47,10 @@ class ContactData extends React.Component {
     render() {
         let form = (
             <form>
-                <input className={classses.Input} type='text' name='name' placeholder='Your name'></input>
-                <input className={classses.Input} type='email' name='email' placeholder='Your e-mail'></input>
-                <input className={classses.Input} type='text' name='street' placeholder='Street'></input>     <input className={classses.Input} type='text' name='postalCode' placeholder='Postal code'></input>
+                <Input inputtype={'input'} type='text' name='name' placeholder='Your name'></Input>
+                <Input inputtype={'input'} name='email' placeholder='Your e-mail'></Input>
+                <Input inputtype={'input'} type='text' name='street' placeholder='Street'></Input>    
+                <Input inputtype={'input'} type='text' name='postalCode' placeholder='Postal code'></Input>
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>    
             </form>
         )
